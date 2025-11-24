acd359bb896af79ebfb14ff51c8da1d61df3e032
fei guo <812773673@qq.com>
2025-11-24 21:32:49 +0800

    fix:host-of-prod

--- lib/api-path.ts
+++ lib/api-path.ts
@@ -1,7 +1,7 @@
 export const isPreview = process.env.NEXT_PUBLIC_IS_PREVIEW === '1';
 export const isProduction = process.env.NODE_ENV === 'production' && !isPreview;
 
-const ProdHost = '';
+const ProdHost = 'https://sb-api.tadle.com ';
 const DevHost = 'https://preview-sandbox-api.tadle.com';
 const Host = isProduction ? ProdHost : DevHost;
 
