e18be2f644ba0f4d69ff2e0a5f2fab5b1587ced1
fei guo <812773673@qq.com>
2025-12-03 16:50:25 +0800

    fix:twitter-asset

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,11 +48,11 @@ export const metadata: Metadata = {
     title: 'Tadle Claim Portal',
     description:
       "Official Tadle airdrop claim. Connect your crypto wallet and web3 social accounts to see if you're eligible for rewards.",
-    url: 'https://claim.tadle.com',
+    url: '/',
     siteName: 'Tadle Claim Portal',
     images: [
       {
-        url: 'https://claim.tadle.com/opengraph-image.jpg?8711bfc4e8de7a39',
+        url: '/opengraph-image.jpg?8711bfc4e8de7a39',
         width: 1200,
         height: 630,
         type: 'image/jpeg',
@@ -68,12 +68,12 @@ export const metadata: Metadata = {
     title: 'Tadle Claim Portal',
     description:
       "Official Tadle airdrop claim. Connect your crypto wallet and web3 social accounts to see if you're eligible for rewards.",
-    images: ['https://claim.tadle.com/opengraph-image.jpg'],
+    images: ['/opengraph-image.jpg'],
   },
   icons: {
     icon: [{ url: '/favicon.ico', sizes: '48x48', type: 'image/x-icon' }, { url: '/favicon.ico' }],
   },
-  metadataBase: new URL('https://claim.tadle.com'),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? 'https://claim.tadle.com'),
 };
 
 export default function RootLayout({
